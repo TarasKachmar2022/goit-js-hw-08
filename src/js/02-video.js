12c7ef7b6d1208f682a9ee3aa96bb7bc70f7651b
Taras Kachmar <kachmar.taras@gmail.com>
2023-02-24 02:43:55 +0200

    done task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(localStorageSaveTime, 2000))
+
+function localStorageSaveTime(currentTime) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));
+};
+
+const savedTime = localStorage.getItem("videoplayer-current-time");
+const parseSavedTime = JSON.parse(savedTime);
+seconds = parseSavedTime.seconds;
+player.setCurrentTime(seconds);
